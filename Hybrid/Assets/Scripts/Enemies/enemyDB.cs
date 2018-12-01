964de899bf1874b58976a0b591dbb432e0b5b4e1
jovlett <44935570+jovlett@users.noreply.github.com>
2018-12-01 13:33:35 -0800

    Version with code
    First pass with all the code

---
+++ Hybrid/Assets/Scripts/Enemies/enemyDB.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Database", menuName = "Database/New enemyDB", order = 1)]
+public class enemyDB : ScriptableObject
+{
+    public GameObject[] enemies;
+    public GameObject healer;
+}
---
+++ Hybrid/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+	IEnumerator Switch (string name) {
+        if (name == "Game")
+            yield return new WaitForSeconds(0.5f);
+
+        SceneManager.LoadScene(name);
+	}
+}
